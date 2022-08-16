2fb68dc0f78d19a1c3cd68c171ab3e537fe8c243
Beka Sturua <beqasturua3@gmail.com>
2022-08-16 13:25:02 +0400

    feat: add reducer

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ function buyCake() {
     info: "First rudex action",
   };
 }
+
+// (previousState, action) => newState
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
